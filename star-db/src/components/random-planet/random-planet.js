7682869fdc6fff3ba3794fb72706bb742ebbd0a2
YuPashintseva <juliyapashintseva@gmail.com>
2021-03-15 11:31:42 +0600

    refactor

--- star-db/src/components/random-planet/random-planet.js
+++ star-db/src/components/random-planet/random-planet.js
@@ -9,7 +9,8 @@ export default class RandomPlanet extends Component {
   SwapiService = new SwapiService();
 
   state = {
-    planet: {}
+    planet: {},
+    loading: true
   }
 
   constructor() {
@@ -18,7 +19,10 @@ export default class RandomPlanet extends Component {
   }
 
   onPlanetLoaded = (planet) => {
-    this.setState({planet});
+    this.setState({
+      planet,
+      loading: false
+    });
   }
 
   updatePlanet() {
@@ -30,10 +34,23 @@ export default class RandomPlanet extends Component {
 
   render() {
 
-    const { planet: {id, name, population, rotationPeriod, diameter} } = this.state;
-
+    const { planet, loading } = this.state;
+    const spinner = loading ? <Spinner /> : null;
+    const content = !loading ? <PlanetView planet={planet} /> : null;
     return (
       <div className="random-planet jumbotron rounded">
+        {spinner}
+        {content}
+      </div>
+    )
+  }
+}
+
+
+const PlanetView = ({ planet }) => {
+  const { id, name, population, rotationPeriod, diameter } = planet;
+  return (
+    <React.Fragment>
         <img className="planet-image"
         src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} />
         <div>
@@ -53,8 +70,6 @@ export default class RandomPlanet extends Component {
             </li>
           </ul>
         </div>
-      </div>
-    )
-  }
+    </React.Fragment>
+  )
 }
-
